d5f6a1ce116fd7c95754787c379b986f62f2e08c
kadriqevani <kadri_qevani@hotmail.com>
2023-03-18 14:42:23 +0100

    all_files

---
+++ database/migrations/2023_03_16_131950_create_matches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->string('team1');
+            $table->string('team2');
+            $table->boolean('isActive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
